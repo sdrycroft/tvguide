ee905267dedd402f61ba4f97a7c2262683301535
Simon Rycroft <github.com@simonrycroft.com>
2015-10-24 01:42:25 +0100

    Moved files into a subfolder.

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 global $conf;
 require_once 'config.inc.php';
-require_once 'lib.inc.php';
+require_once 'lib/functions.inc.php';
 if(php_sapi_name() != 'cli'){
   // If we are not on the command line, we output the HTML
-  require_once 'view.inc.php';
+  require_once 'lib/view.inc.php';
 } else {
   // If we are being called from the command line, we do the load.
-  require_once 'load.inc.php';
+  require_once 'lib/load.inc.php';
 }
